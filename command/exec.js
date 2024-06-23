751a7d2eca3ea5b1cf7d107a7506f7d9b356db9b
ArdianROR <ardianyosepaaa@gmail.com>
2024-06-23 17:30:40 +0700

    Add files via upload

---
+++ command/exec.js
@@ -0,0 +1,50 @@
+import path from "path";
+import util from "util";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+import cp, { exec as _exec } from "child_process";
+let exec = util.promisify(_exec).bind(cp);
+
+export async function before(m) {
+  if (m.isBaileys) return;
+  if (!m.isOwner) return;
+
+  if ([">", "=>"].some((a) => m.body.toLowerCase().startsWith(a))) {
+    let __dirname = path.dirname(fileURLToPath(import.meta.url));
+    let require = createRequire(__dirname),
+      _return = "";
+
+    try {
+      _return = /await/i.test(m.text)
+        ? eval("(async() => { " + m.text + " })()")
+        : eval(m.text);
+    } catch (e) {
+      _return = e;
+    }
+
+    new Promise((resolve, reject) => {
+      try {
+        resolve(_return);
+      } catch (err) {
+        reject(err);
+      }
+    })
+      ?.then((res) => m.reply(func.format(res)))
+      ?.catch((err) => m.reply(func.format(err)));
+  }
+
+  if (["$", "exec"].some((a) => m.body.toLowerCase().startsWith(a))) {
+    let o;
+    try {
+      o = await exec(m.text);
+    } catch (e) {
+      o = e;
+    } finally {
+      let { stdout, stderr } = o;
+      if (typeof stdout === "string" && stdout.trim()) m.reply(stdout);
+      if (typeof stderr === "string" && stderr.trim()) m.reply(stderr);
+    }
+  }
+
+  return !0;
+}
